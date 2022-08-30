dbd0b7b4f87c596cb9a066144e33d54c5d8bb6a3
Nate Foss <npfoss@gmail.com>
2022-08-30 14:52:37 -0400

    getMap example

--- get-map.js
+++ get-map.js
@@ -1,16 +1,16 @@
 const axios = require("axios");
 
 const { SPACE_ID, API_KEY } = require("./config");
-const MAP_ID = "custom-entrance";
+const MAP_ID = "test";
 
-await axios.get("https://gather.town/api/setMap", {
-    apiKey: API_KEY,
-    spaceId: SPACE_ID,
-    mapId: MAP_ID,
-    mapContent: Object.assign(BASE_MAP, {
-        objects: BASE_MAP.objects.concat(posters).concat(zoomZones),
-        spaces: privateSpaces,
-        collisions: new Buffer(collBytes).toString("base64"),
-        // ^ base64 encoded array of dimensions[1] x dimensions[0] bytes (each either 0x00 or 0x01)
-    }),
-});
+"/spaces/:space/maps/:map"
+
+
+axios.get(
+    `https://api.gather.town/api/v2/spaces/${encodeURIComponent(SPACE_ID)}/maps/${encodeURIComponent(MAP_ID)}`,
+        {
+            headers: {
+                apiKey: API_KEY,
+            },
+        },
+).then((response) => console.log(response.data)).catch(console.error);
